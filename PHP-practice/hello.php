08d6a542b0beec2f7634113cabd0c41fc07bdf16
AsifHimu <asifbinkabircse@gmail.com>
2022-08-25 18:38:54 +0600

    new php prractice file added

---
+++ PHP-practice/hello.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $text = 'PHP file';
+        $color = "red";
+        echo "My first $text<br>";
+        ECHO "Hello World $color!<br>";
+        echo "Hello World $color!<br>";
+        echo "hello ",$text;
+        //comment
+        #comment
+        /*
+        multiline comment
+        */
+        echo "<br>";
+        echo 5/7;
+    ?>
+</body>
+</html>
---
+++ PHP-practice/practice1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice1</title>
+</head>
+<body>
+    <?php
+        echo 5+3444444448888880,"<br>";
+        $val = 9/11;
+        echo $val,"<br>";
+        $txt = "W3Schools.com";
+        echo "I love " , $val . "!","<br>";
+
+        $x = 5;
+        function test($z) {
+            $y = 89;
+            echo "<p>Inside func value: $y $z</p>";
+        }
+        test(10);
+        echo "<p>Outside func x value: $x</p>";
+    ?>
+</body>
+</html>
